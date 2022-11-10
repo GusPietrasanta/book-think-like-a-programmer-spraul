c50836c48f4f76b801fce59a7a81ac049d8e6211
GusPietrasanta <gusdsm@gmail.com>
2022-11-10 20:12:07 +1030

    Luhn Checksum Validation - Book solution added

--- luhnChecksumBookSolution.cpp
+++ luhnChecksumBookSolution.cpp
@@ -10,11 +10,46 @@ int oddLengthChecksum = 0;
 int evenLengthChecksum = 0;
 int position = 1;
 
+int doubleDigitValue(int digitToDouble);
+
 int main(){
     std::cout << "Enter a number: ";
     digit = std::cin.get();
     // While digit is not end of line
     while (digit != 10){
-
+        if(position % 2 == 0){
+            oddLengthChecksum += doubleDigitValue(digit - '0');
+            evenLengthChecksum += digit - '0';
+        }
+        else{
+            oddLengthChecksum += digit - '0';
+            evenLengthChecksum += doubleDigitValue(digit - '0');
+        }
+        digit = std::cin.get();
+        position++;
     }
+    int checksum;
+    if ((position - 1) % 2 == 0){
+        checksum = evenLengthChecksum;
+    }
+    else{
+        checksum = oddLengthChecksum;
+    }
+    std::cout << "Checksum is " << checksum << ". \n";
+
+    if(checksum % 10 == 0){
+        std::cout << "Checksum is divisible by 10. Valid.\n";
+    }
+    else{
+        std::cout << "Checksum is not divisible by 10. Invalid.\n";
+    }
+
+}
+
+int doubleDigitValue(int digitToDouble){
+    int doubleDigit = digitToDouble * 2;
+    if(doubleDigit >= 10){
+        doubleDigit = 1 + (doubleDigit - 10);
+    }
+    return doubleDigit;
 }
